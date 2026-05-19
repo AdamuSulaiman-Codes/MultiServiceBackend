3cbdd40edff4fdc6deab209912e4411e7b4ed17f
adamusulaimanmuhammad <sulaimancodes2235@gmail.com>
2026-05-19 15:03:10 +0100

    added to github

---
+++ subscriber-service/src/main/java/com/achlys20/SubscriberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.achlys20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SubscriberServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriberServiceApplication.class, args);
+    }
+}
